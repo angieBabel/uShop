0a80cd4387473cdf24426b95e1c5b2e62d4fce0f
irvinerp <irvinerp@gmail.com>
2015-03-26 10:02:33 -0600

    Registro & login creados

--- UShop/app/src/main/java/com/irvin/ushop/MainActivity.java
+++ UShop/app/src/main/java/com/irvin/ushop/MainActivity.java
@@ -4,6 +4,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseUser;
+import com.parse.ParseException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +22,40 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "Y4Z18hEnQtZYFxpMju1cvethHHtKZfu9aS4QyIag", "oB4lTch3tn57FeXB6dYdCgBIZfkKveTaYkkZeLDV");
+
     }
 
+    public void ingresarUsuario(View view){
+
+        //Metodo para obtener el nombre de usuario ingresado
+        EditText usr = (EditText )findViewById(R.id.nombre_et);
+        String usuario = usr.getText().toString();
+        //Obtencion de la contraseña ingresada
+        EditText pasw = (EditText)findViewById(R.id.passw_et);
+        String contrasena = pasw.getText().toString();
+
+        //Pase de parametros enviados a parse para ingresar
+        ParseUser.logInInBackground(usuario, contrasena, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Hooray! The user is logged in.
+                    Intent i = new Intent(getApplicationContext(),MenuShop.class);
+                    startActivity(i);
+                } else {
+                    // Signup failed. Look at the ParseException to see what happened.
+                    Toast.makeText(getApplication(),"Fallo inicio de sesión",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void mostrarVistaRegistro(View view) {
+        Intent i = new Intent(this, Registro.class );
+        startActivity(i);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ UShop/app/src/main/java/com/irvin/ushop/Registro.java
@@ -0,0 +1,80 @@
+package com.irvin.ushop;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.parse.ParseException;
+import android.view.View;
+import android.widget.Toast;
+
+public class Registro extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registro, menu);
+        return true;
+    }
+
+    public void enviarRegistro(View view) {
+
+        EditText username1 = (EditText) findViewById(R.id.usernamereg);
+        String username = username1.getText().toString();
+
+        EditText pasw1 = (EditText) findViewById(R.id.paswreg);
+        String password = pasw1.getText().toString();
+
+        EditText email1 = (EditText) findViewById(R.id.emailreg);
+        String email = email1.getText().toString();
+
+        EditText phone1 = (EditText) findViewById(R.id.telefonoreg);
+        String phone = phone1.getText().toString();
+
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        //"650-253-0000"
+        user.put("phone", phone);
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    //Toast.makeText(getApplication(),"Hooray! Let them use the app now.",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(),MenuShop.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplication(),"Fallo el registro. Revise los campos.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
